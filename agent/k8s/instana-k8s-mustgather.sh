6e8052182c1fcb65f54e47326e9322a84a012e99
Konrad Ohms <konrad.ohms@de.ibm.com>
2024-12-05 09:50:33 +0100

    feat: Initial dump of existing instana-k8s-mustgather_1.sh
    Dump of current version from https://www.ibm.com/support/pages/how-collect-instana-agent-doc-red-hat-openshift-and-kubernetes-environments

---
+++ agent/k8s/instana-k8s-mustgather.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+###############################################################################
+#
+# This script is used to collect data for
+# the Instana Host Agent on Kubernetes / Openshift
+#
+# ./instana-k8s-mustgather.sh
+#
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+export MGDIR=instana-mustgather-$current_time
+mkdir -p $MGDIR
+if command -v oc > /dev/null 
+then
+  CMD=oc
+  LIST_NS="instana-agent openshift-controller-manager"
+else
+  CMD=kubectl
+  LIST_NS="instana-agent"
+fi
+
+$CMD get nodes > $MGDIR/node-list.txt
+$CMD describe nodes > $MGDIR/node-describe.txt
+$CMD get namespaces > $MGDIR/namespaces.txt
+$CMD describe cm instana-agent -n instana-agent > $MGDIR/configMap.txt
+
+if [ $CMD == "oc" ] 
+then
+  $CMD get clusteroperators > $MGDIR/cluster-operators.txt
+fi
+$CMD get pods -n instana-agent -o wide > $MGDIR/instana-agent-pod-list.txt
+if [ $CMD == "oc" ] 
+then
+  $CMD get pods -n openshift-controller-manager -o wide > $MGDIR/openshift-controller-manager-pod-list.txt
+fi
+for NS in $LIST_NS; do
+  export NS=$NS
+  mkdir $MGDIR/$NS
+  $CMD get all,events -n $NS -o wide &> $MGDIR/$NS/all-list.txt
+  $CMD get pods -n $NS | awk 'NR>1{print "'$CMD' -n $NS describe pod "$1" > $MGDIR/$NS/"$1"-describe.txt && echo described "$1}' | bash
+  $CMD get pods -n $NS -o go-template='{{range $i := .items}}{{range $c := $i.spec.containers}}{{println $i.metadata.name $c.name}}{{end}}{{end}}' > $MGDIR/$NS/container-list.txt
+  awk '{print "'$CMD' -n $NS logs "$1" -c "$2" --tail=10000 > $MGDIR/$NS/"$1"_"$2".log && echo gathered logs of "$1"_"$2}' $MGDIR/$NS/container-list.txt | bash
+  awk '{print "'$CMD' -n $NS logs "$1" -c "$2" --tail=10000 -p > $MGDIR/$NS/"$1"_"$2"_previous.log && echo gathered previous logs of "$1"_"$2}' $MGDIR/$NS/container-list.txt | bash
+done
+tar czf $MGDIR.tgz $MGDIR/
